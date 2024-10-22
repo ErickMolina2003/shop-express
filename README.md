d7b0246c5d355644e23c0e15127843fec6e8f80d
eralejo2003 <eralejo2003@gmail.com>
2024-10-21 23:19:52 -0600

    updating env file & readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # shop-express
+1. npm install
+2. copiar .env.example a .env
+3. crear un cluster de mongodb
+4. conectarse al cluster de mongodb agregando nuestra ip al network list de mongodb
+5. conectarse al cluster de mongodb con MongoDB Driver
+6. Reemplazar el MONGO_URL del .env con el connection string de nuestro cluster de mongodb
--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,20 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 import router from './router';
 
+dotenv.config();
+
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: 'http://localhost:3000',
     credentials: true,
-}));
+  })
+);
 
 app.use(compression());
 app.use(cookieParser());
@@ -22,16 +27,13 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 
 server.listen(8080, () => {
-    console.log('Server is running on http://localhost:8080');
-})
+  console.log('Server is running on http://localhost:8080');
+});
 
-const username = encodeURIComponent("erick");
-const password = encodeURIComponent("erick");
-
-const MONGO_URL = `mongodb+srv://${username}:${password}@cluster0.0zxopwv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
 
-app.use('/', router())
+app.use('/', router());
